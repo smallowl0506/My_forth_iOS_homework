180b0faa3b8ef557657b9a67ccd1a5afd23b8978
jubao <tqy_4896@hotmail.com>
2020-05-07 21:28:35 +0800

    各景點評分分析增加長條圖選項

--- My_forth_iOS_homework/chart.swift
+++ My_forth_iOS_homework/chart.swift
@@ -26,6 +26,8 @@ struct Chart: View {
     @ObservedObject var sightData = SightData()
     var angles: [Angle]
     var scoreCount: [Double] = [0,0,0,0,0,0]
+    @State private var selectedChart = "圓餅圖"
+    var analyzeChart = ["圓餅圖", "柱狀圖"]
     init (sightData: SightData)
     {
         var sum: Double = 0
@@ -46,11 +48,27 @@ struct Chart: View {
     }
     
     var body: some View {
-        NavigationView{
-            VStack{
-                DrawPieChart(angles: angles).padding(.top,20)
-            }.navigationBarTitle("各名勝景點評分分佈")
-        }
+         NavigationView{
+                   VStack{
+                       Picker(selection: $selectedChart, label: Text("請選擇分析圖表")) {
+                            ForEach(self.analyzeChart, id: \.self) { (selectedChart) in
+                                Text(selectedChart)
+                            }
+                        }
+                        .labelsHidden()
+                        .pickerStyle(SegmentedPickerStyle())
+                        .position(x: 210, y: 0)
+                        .navigationBarTitle(Text("各大景點評分分佈圖"))
+                       if self.selectedChart == "圓餅圖" {
+                           DrawPieChart(angles: angles).padding(.top,20)
+                       }
+                       else if self.selectedChart == "柱狀圖" {
+                           DrawBarChart(scoreCount: scoreCount)
+                               .frame(width: 400, height: 600, alignment: .bottom)
+                       }
+                   }
+                   .padding(.top,20)
+               }
         
     }
 }
@@ -83,7 +101,7 @@ struct DrawPieChart: View {
                     Text("０分")
                 }
             }
-            .padding(.top,50)
+            //.padding(.top,50)
             .frame(width:350, height: 30)
             ZStack{
                 PieChart(startAngle:self.angles[5], endAngle: .zero)
@@ -98,6 +116,83 @@ struct DrawPieChart: View {
                     .fill(Color.gray)
                 PieChart(startAngle:self.angles[0], endAngle: self.angles[1])
                     .fill(Color.black)
+            }
+        }
+        .padding(.top,-250)
+    }
+}
+
+struct DrawBarChart: View {
+    var scoreCount: [Double]
+    var body: some View {
+        HStack
+        {
+            ZStack
+            {
+                DrawLine()
+                CountText().offset(x: -150)
+                BarChart(scoreCount: scoreCount).offset(y:85)
+            }
+        }
+    }
+}
+
+struct DrawLine: View
+{
+    var body: some View
+    {
+       Path{ (path) in
+            path.move(to: CGPoint(x: 75, y: 150))
+            path.addLine(to: CGPoint(x: 75, y: 465))
+            path.addLine(to: CGPoint(x: 375, y: 465))
+        }
+        .stroke(Color.black, lineWidth: 2)
+    }
+}
+
+struct CountText: View
+{
+    var body: some View
+    {
+        Group {
+            Text("0").offset(x:10, y:170)
+            Text("5").offset(x:10, y:115)
+            Text("10").offset(x:10, y:65)
+            Text("15").offset(x:10, y:15)
+            Text("20").offset(x:10, y:-35)
+            Text("25").offset(x:10, y:-85)
+            Text("30").offset(x:10, y:-135)
+        }
+        .font(Font.system(size: 25))
+    }
+}
+struct BarChart: View {
+    @State private var height: [CGFloat] = [0,0,0,0,0,0,0]
+    var scoreCount: [Double] = [0,0,0,0,0,0]
+    var body: some View
+    {
+        HStack{
+            ForEach(0..<scoreCount.count){ (index) in
+                VStack{
+                    Text(String(Int(self.scoreCount[index])))
+                    Rectangle()
+                        .fill(Color.red)
+                        .frame(width: 25, height: self.height[index])
+                        .animation(.linear(duration: 1))
+                        .onAppear
+                        {
+                            self.height[index] = CGFloat(self.scoreCount[index]) * 10
+                        }
+                    Text(String(index))
+                        .font(Font.system(size: 25))
+                    Image(systemName: "star.fill")
+                        .resizable()
+                        .frame(width: 20, height:20)
+                        .foregroundColor(Color.yellow)
+                        .offset(y:-10)
+                }
+                .padding(.trailing,10)
+                .offset(x:10, y: 102 + CGFloat(self.scoreCount[index] * -5))
             }
         }
     }
